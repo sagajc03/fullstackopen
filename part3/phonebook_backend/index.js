ed08a0f5b9b89a8f8fd32c7dbee60163f3442fa3
sagajc03 <sagajc03@gmail.com>
2025-09-06 19:36:49 -0600

    Built front of phonebook

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -27,6 +27,7 @@ let persons = [
 
 app.use(express.json());
 app.use(morgan("tiny"));
+app.use(express.static("dist"));
 
 app.get("/info", (request, response) => {
   const ahora = Date.now();
@@ -105,7 +106,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
